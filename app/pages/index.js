d12ad3f013b22f303a9b859cc3bf27b901a7a58f
Christina <christinaljchen@gmail.com>
2022-05-03 16:45:34 -0400

    fix class errors

--- app/pages/index.js
+++ app/pages/index.js
@@ -59,30 +59,30 @@ export default function Home() {
       <main className>
 
         <div className="container">
-        <nav class="navbar mt-3" role="navigation" aria-label="main navigation">
-          <div class="navbar-menu"><div class="navbar-start">
-            <div class="navbar-item"><strong>Group Saving dApp</strong></div></div></div>
+        <nav className="navbar mt-3" role="navigation" aria-label="main navigation">
+          <div className="navbar-menu"><div className="navbar-start">
+            <div className="navbar-item"><strong>Group Saving dApp</strong></div></div></div>
 
-          <div class="navbar-end"><div class="navbar-item">
-            <div class="buttons">
+          <div className="navbar-end"><div className="navbar-item">
+            <div className="buttons">
             <button onClick={connectWalletHandler} className="button is-ghost">Connect Wallet</button>
             <button className="button is-link">Contribute 0.01 ETH</button>
               </div></div></div>
         </nav>
         
-        <div class="notification mx-3 mt-3 mb-6">
+        <div className="notification mx-3 mt-3 mb-6">
           Hello, welcome to version 1 of the group saving decentralized app, based on the concept of ROSCAs (Rotating Savings and Credit Association). Here’s how it works: four people contribute 0.01 ETH every week for four weeks. Each week, one member of the group will receive the entire balance, following the order of transactions. Future features may include: interest, custom rules, create your own groups, and more.  
         </div>
 
         <div className="columns">
           <div className="column is-7 mx-3">
             <p>SAVINGS</p><break></break>
-            <h1 class="title">0.01 ETH</h1>
+            <h1 className="title">0.01 ETH</h1>
           </div> 
 
           <div className="column is-5 mb-4 mx-3">
             <p>MEMBERS</p>
-            <ol class="ml-4">
+            <ol className="ml-4">
               <li>0x1c73d1074d779a7b64fede19313ac53c8fd533bf</li>
               <li>0xf91A8511487E78Bf4dB3c2B10b566B92B430ecC7</li>
               <li>0xef10899B6C17747381C66Bc1BFD48679Fa682c35</li>
@@ -91,8 +91,8 @@ export default function Home() {
           </div>
         </div>
 
-        <div class="table-container mx-3 mb-6">
-          <table class="table is-fullwidth">
+        <div className="table-container mx-3 mb-6">
+          <table className="table is-fullwidth">
             <thead>
               <tr>
                 <th>Date</th>
